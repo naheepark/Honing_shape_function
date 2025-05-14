87f59242bf315224f5e754ea790c3c84051d1cd6
naheepark <nhpark@wta.kr>
2025-05-14 15:40:47 +0900

    파이썬 파일 추가

---
+++ calculate/calculate.py
@@ -0,0 +1,55 @@
+﻿
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import os
+import glob
+import math
+
+# 한글 폰트 설정
+mpl.rc('font', family='Malgun Gothic')
+mpl.rcParams['axes.unicode_minus'] = False
+
+
+with open('C:\\Users\\박나희\\Desktop\\Honing_shape_function\\contour_0.gen', 'r') as f:
+    lines = f.readlines()
+    
+data_lines = lines[1:-2]
+
+x = []
+y = []
+
+for line in data_lines:
+    parts = line.strip().split()
+    if len(parts) != 2:
+        continue
+    x_val, y_val = map(float, parts)
+    x.append(x_val)
+    y.append(y_val)
+      
+
+coordinates = list(zip(x, y))      
+x_min = min(coordinates, key = lambda x: x[0])[0]
+y_max = max(coordinates, key = lambda y: y[1])[1]
+
+target = (x_min, y_max)
+min_dist = float('inf')
+closest_point = None
+
+for k in coordinates:
+    dist = math.dist(target, k)
+    if dist < min_dist:
+        min_dist = dist
+        closest_point = k
+
+print(closest_point)
+
+# 시각화
+plt.plot(x, y, linewidth=3, label='원본 곡선')
+plt.axvline(x=x_min, color='red', linestyle='--', label=f'x={x_min:.2f}')
+plt.axhline(y=y_max, color='green', linestyle='--', label=f'y={y_max:.2f}')
+plt.scatter(*closest_point, color='blue', label='가장 가까운 점', zorder=5)
+plt.xlabel("X 축")
+plt.ylabel("Y 축")
+plt.grid(True)
+plt.legend()
+plt.show()
---
+++ visualization/visualization.py
@@ -0,0 +1,46 @@
+﻿import matplotlib.pyplot as plt
+import matplotlib as mpl
+import os
+import glob
+
+# 한글 폰트 설정
+mpl.rc('font', family='Malgun Gothic')
+mpl.rcParams['axes.unicode_minus'] = False
+
+# .gen 파일 목록 가져오기
+gen_files = glob.glob(r"C:\Users\박나희\Desktop\Honing_shape_function\*.gen")
+
+# 저장할 폴더 경로
+save_folder = r"C:\Users\박나희\Desktop\Honing_shape_function\visualization\graph"
+os.makedirs(save_folder, exist_ok=True)
+
+for gen_file in gen_files:
+    x, y = [], []
+
+    with open(gen_file, "r", encoding="cp949") as f:
+        lines = f.readlines()
+
+    data_lines = lines[1:-2]
+
+    for line in data_lines:
+        parts = line.strip().split()
+        if len(parts) != 2:
+            continue
+        x_val, y_val = map(float, parts)
+        x.append(x_val)
+        y.append(y_val)
+
+    # 그래프 그리기
+    plt.figure()
+    plt.plot(x, y, marker='o', linestyle='--', linewidth=0.7, markersize=2)
+    plt.xlabel("X 값")
+    plt.ylabel("Y 값")
+    plt.title(os.path.basename(gen_file))  # 제목은 전체 파일명
+
+    plt.grid(True)
+
+    # 파일명만 추출해서 저장
+    filename = os.path.splitext(os.path.basename(gen_file))[0]
+    save_path = os.path.join(save_folder, filename + ".png")
+    plt.savefig(save_path, dpi=300)
+    plt.close()
